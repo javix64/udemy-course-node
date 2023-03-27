55f801240e3ba47bdb9c036ceb68f79f27313a8f
javix64 <jav1.x64@gmail.com>
2023-03-27 21:37:11 +0200

    [ENH] Finish section11

--- section8-12-RestServer/controllers/auth.js
+++ section8-12-RestServer/controllers/auth.js
@@ -2,22 +2,23 @@ const { request, response } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const { generateJWT } = require('../helpers/generateJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 
-const login = async (req=request,res=response) => {
+const login = async (req = request, res = response) => {
     const { email, password } = req.body;
-    try{
+    try {
         // Check if email exist
-        const user = await User.findOne({email});
-        if(!user) return res.status(400).json({msg: 'User / Password is wrong - email'});
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ msg: 'User / Password is wrong - email' });
 
         // Check if user is active
-        if(!user.status) return res.status(400).json({msg:'User / Password is not correct - status:false'});
-        
+        if (!user.status) return res.status(400).json({ msg: 'User / Password is not correct - status:false' });
+
         // Check password
         const validPassword = bcryptjs.compareSync(password, user.password);
-        if(!validPassword) return res.status(400).json({msg:'User / Password is not correct - password'});
+        if (!validPassword) return res.status(400).json({ msg: 'User / Password is not correct - password' });
 
         // Generate JWT
         const token = await generateJWT(user.id);
@@ -28,16 +29,60 @@ const login = async (req=request,res=response) => {
             token
         })
 
-    }catch(err){
-        console.error('error Login controller',err)
+    } catch (err) {
+        console.error('error Login controller', err)
         return res.status(500).json({
-            msg:'Talk with webmaster'
+            msg: 'Talk with webmaster'
         })
     }
 
 };
 
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+    try {
+
+        const { name, img, email } = await googleVerify(id_token);
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            console.info('entro?')
+            // Create user if is not registered;
+            const data = {
+                name,
+                email,
+                img,
+                password: 'somePassword',
+                google: true,
+                rol:'USER_ROLE'
+            };
+            user = new User( data );
+            await user.save();
+        }
+        // If user is already registered
+
+        // If user is in DB
+        if (!user.status) return res.status(401).json({ msg: 'Talk with webmaster, user blocked' });
+        const token = await generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            ok: false,
+            msg: 'Token could not verified',
+            err
+        })
+    }
+
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- section8-12-RestServer/routes/auth.js
+++ section8-12-RestServer/routes/auth.js
@@ -1,15 +1,20 @@
 const { Router } = require('express');
-const {check} = require('express-validator');
-const { login } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 
-router.post('/login',[
+router.post('/login', [
     check('email', 'Email is required').isEmail(),
-    check('password','Password is required').not().isEmpty(),
-    validateFields 
+    check('password', 'Password is required').not().isEmpty(),
+    validateFields
 ], login);
+
+router.post('/google', [
+    check('id_token', 'id_token is required').not().isEmpty(),
+    validateFields
+], googleSignIn);
 
 module.exports = router;
